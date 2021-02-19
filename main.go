58dd9f78ed0f7b6bce07d5e7a4e259c5483304cf
Lauro Pereira <lauro.s.pereira@gmail.com>
2021-02-19 01:46:34 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Teste 1</h1>")
+}
+
+func main(){
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
